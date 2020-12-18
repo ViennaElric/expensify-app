bc8ab609ca8a7be91e77629c1070cc1af6d1b8e3
vienna <viennaelric@gmail.com>
2020-12-18 10:48:19 -0500

    the complete react developer course: Using a third-party component

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp.jsx';
+
+//this file is in charge of bootstrapping things that live elsewhere
+  
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
---
+++ webpack.config.jsx
@@ -0,0 +1,26 @@
+
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.jsx'
+    },
+
+    module: {
+        rules: [{
+                loader: 'babel-loader',
+                test: /\.jsx$/,
+                exclude: /node_modules/
+        }]
+    },
+
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+
+};
+
