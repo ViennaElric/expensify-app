983755926c858f9f6c7be638d054257bcabd427f
vienna <viennaelric@gmail.com>
2021-04-01 15:41:39 -0600

    styled the app

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,3 @@
-
- 
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
@@ -7,18 +5,22 @@ import { startAddExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-    //the component should be completely unaware of where the data is coming from and where its really going to
-      // the component should be concerned with the presentation of info and basic user interaction
     this.props.startAddExpense(expense);
     this.props.history.push('/');
   };
   render() {
     return (
       <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm
-          onSubmit={this.onSubmit}
-        />
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Add Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
+            onSubmit={this.onSubmit}
+          />
+        </div>
       </div>
     );
   }
